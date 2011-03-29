90bf93fdcdbd173e5173122c9ade8ce2ad1b8ecf
Pistos <gitsomegrace.5.pistos@geoshell.com>
2011-03-29 10:43:51 -0400

    Initial commit for Rally extension.

--- story-id.rb
+++ story-id.rb
@@ -3,10 +3,10 @@ story_id_proc = Proc.new do |buffer|
     dir = File.dirname( File.dirname( File.expand_path( buffer.name ) ) )
     branch = Dir.chdir( dir ){ `git symbolic-ref HEAD 2>/dev/null`[ /[^\/\n]+$/ ] }
     if branch
-      story_id = branch[ /^(\d{6,})-/, 1 ]
+      story_id = branch[ /^((?:us|de)\d{2,})-/i, 1 ].upcase
       if story_id
-        if buffer.current_line !~ /\[##{story_id}\]/
-          buffer.paste "[##{story_id}] "
+        if buffer.current_line !~ /\[#{story_id}\]/
+          buffer.paste "[#{story_id}] "
         end
       end
     end
