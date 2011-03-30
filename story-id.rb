30658d884dd416948fd263de8fc3517106c61a6e
Pistos <gitsomegrace.5.pistos@geoshell.com>
2011-03-30 12:57:00 -0400

    Bugfix: graceful handling of case where there is no story or defect ID in the branch name.

--- story-id.rb
+++ story-id.rb
@@ -3,8 +3,9 @@ story_id_proc = Proc.new do |buffer|
     dir = File.dirname( File.dirname( File.expand_path( buffer.name ) ) )
     branch = Dir.chdir( dir ){ `git symbolic-ref HEAD 2>/dev/null`[ /[^\/\n]+$/ ] }
     if branch
-      story_id = branch[ /^((?:us|de)\d{2,})-/i, 1 ].upcase
+      story_id = branch[ /^((?:us|de)\d{2,})-/i, 1 ]
       if story_id
+        story_id.upcase!
         if buffer.current_line !~ /\[#{story_id}\]/
           buffer.paste "[#{story_id}] "
         end
