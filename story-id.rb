c996d6dedf390389634dd89e4964946e15872e39
Pistos <github.pistos@purepistos.net>
2014-02-19 14:56:09 -0500

    Initial commit.

--- story-id.rb
+++ story-id.rb
@@ -1,17 +1,14 @@
-story_id_proc = Proc.new do |buffer|
+card_id_proc = Proc.new do |buffer|
   if buffer && buffer.name =~ %r{/.git/COMMIT_EDITMSG$}
     dir = File.dirname( File.dirname( File.expand_path( buffer.name ) ) )
     branch = Dir.chdir( dir ){ `git symbolic-ref HEAD 2>/dev/null`[ /[^\/\n]+$/ ] }
     if branch
-      story_id = branch[ /^((?:us|de)\d{2,})-/i, 1 ]
-      if story_id
-        story_id.upcase!
-        if buffer.current_line !~ /\[#{story_id}\]/
-          buffer.paste "[#{story_id}] "
-        end
+      card_id = branch[ /^TR-([A-Za-z0-9]{8,})-/i, 1 ]
+      if card_id && buffer.current_line !~ /\[#{card_id}\]/
+        buffer.paste "[#{card_id}] "
       end
     end
   end
 end
 
-$diakonos.register_proc( story_id_proc, :after_open )
+$diakonos.register_proc( card_id_proc, :after_open )
